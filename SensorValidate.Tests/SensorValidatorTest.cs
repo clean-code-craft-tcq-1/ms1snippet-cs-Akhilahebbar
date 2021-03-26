f8b395e74eee9c645231e3cec1cede049774a918
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-03-26 08:58:46 +0530

    Update SensorValidatorTest.cs

--- SensorValidate.Tests/SensorValidatorTest.cs
+++ SensorValidate.Tests/SensorValidatorTest.cs
@@ -8,14 +8,14 @@ namespace SensorValidate.Tests
     public class SensorValidatorTest
     {
         [Fact]
-        public void reportsErrorWhenSOCjumps() {
-            Assert.False(SensorValidator.validateSOCreadings(
+        public void ReportsErrorWhenSOCjumps() {
+            Assert.False(SensorValidator.ValidateSOCreadings(
                 new List<double>{0.0, 0.01, 0.5, 0.51}
             ));
         }
         [Fact]
-        public void reportsErrorWhenCurrentjumps() {
-            Assert.False(SensorValidator.validateCurrentreadings(
+        public void ReportsErrorWhenCurrentjumps() {
+            Assert.False(SensorValidator.ValidateCurrentreadings(
                 new List<double>{0.03, 0.03, 0.03, 0.33}
             ));
         }
