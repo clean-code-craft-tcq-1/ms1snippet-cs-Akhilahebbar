c9a191f2c280fba60408a5cde41243834d046b05
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-03-26 08:57:55 +0530

    Update SensorValidate.cs

--- SensorValidate/SensorValidate.cs
+++ SensorValidate/SensorValidate.cs
@@ -8,13 +8,13 @@ namespace SensorValidate
         public static bool CheckValueJumps(double value, double nextValue, double maxDelta) {
             return nextValue - value > maxDelta ? false : true;
         }
-        public static bool validateSOCreadings(List<Double> values) {
+        public static bool ValidateSOCreadings(List<Double> values) {
             return validateReadings(values,0.05);
         }
-        public static bool validateCurrentreadings(List<Double> values) {
-            return validateReadings(values,0.1);
+        public static bool ValidateCurrentreadings(List<Double> values) {
+            return ValidateReadings(values,0.1);
         }
-        public static bool validateReadings(List<Double> values,Double maxDelta)
+        public static bool ValidateReadings(List<Double> values,Double maxDelta)
         {
             for (int i = 0; i < values.Count - 1; i++)
             {
